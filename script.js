8e9d6029c0bc32983599143c8320694036d51a91
abhisingh3960 <94777478+abhisingh3960@users.noreply.github.com>
2024-09-28 22:42:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
- function calculateArea() {
-            // Get the radius from the user using the prompt() function
-            let radius = prompt("Enter the radius of the circle:");
-            // write you code here and display the result to the user
-           
+function calculateArea() {
+     let radius = parseFloat(prompt("Enter the radius of the circle:"));
+
+    // Check if the input is a valid positive number
+    if (radius > 0) {
+       let area = Math.PI * Math.pow(radius, 2);
+
+         let roundedArea = area.toFixed(2);
+ alert(`The area of the circle with radius ${radius} is ${roundedArea}`);
+    } else {
+        alert("Please enter a valid positive number for the radius.");
+    }
 }
+
+// Call the function
 calculateArea();
